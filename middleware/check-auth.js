6d2e319b9875f66321a79128fd76a3b5eceaded4
Vyacheslav Bogdanyonok <skavik46111@gmail.com>
2023-02-11 21:23:00 +0300

    RequiresAuth now actually working

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,6 +1,11 @@
-export default async ({ store, redirect, app, route, error }) => {
+export default async ({ store, redirect, app, route }) => {
   const token = app.$cookies.get('token')
   const user = store.getters['users/checkAuth']
+  const requiresAuth = route.meta[0].requiresAuth || false
+
+  if(requiresAuth && !user) {
+    redirect('/login')
+  }
 
   if(token && !user){
     try {
@@ -11,5 +16,12 @@ export default async ({ store, redirect, app, route, error }) => {
       await store.commit('users/SET_CURRENT_USER', undefined)
       redirect('/login')
     }
+  }
+
+  if(route.requiresAuth && !user) {
+    app.$cookies.remove('token')
+    await store.commit('users/SET_ACCESS_TOKEN', undefined)
+    await store.commit('users/SET_CURRENT_USER', undefined)
+    redirect('/login')
   }
 }
