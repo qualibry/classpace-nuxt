75cf4fcf9cee30f77c7ae4f02bcf80af27ae8dd5
@pavel <pavelkukuskmn388@gmail.com>
2023-04-10 15:15:26 +0300

    feat: add loading items via infinite scroll

--- components/rooms/PostList.vue
+++ components/rooms/PostList.vue
@@ -1,109 +1,174 @@
 <template>
-    <div class="attachments-block">
-        <div
-            v-for="item in typedPosts"
-            class="bg-white border-solid border-1 p-3 border-round-lg border-300 mb-4 block"
-            :key="item.id"
-        >
-            <div @click="redirectDetail(item)" class="block-nav">
-                <div class="flex align-items-center">
-                    <i class="pi pi-file" style="font-size:1.8rem;margin-right:1.3rem"></i>
-                    <div>
-                        <div class="flex align-items-center">
-                            <h4 class="mt-0 mb-2 mr-auto">{{ item.title }}</h4>
-                        </div>
-                        <span>{{ item.description }}</span>
-                    </div>
-                </div>
-                <Divider />
-                <div class="flex align-items-center text-sm">
-                    <span class="time text-gray-500">{{ new Date(item.created_at) | dateFormat('YYYY.MM.DD') }}</span>
-                    <span class="files ml-auto">{{ item.attachments_count }} {{ $t('posts.filesAttached') }}</span>
-                </div>
+  <div class="attachments-block">
+    <div
+      v-for="item in items"
+      class="bg-white border-solid border-1 p-3 border-round-lg border-300 mb-4 block"
+      :key="item.id"
+    >
+      <div @click="redirectDetail(item)" class="block-nav">
+        <div class="flex align-items-center">
+          <i
+            class="pi pi-file"
+            style="font-size: 1.8rem; margin-right: 1.3rem"
+          ></i>
+          <div>
+            <div class="flex align-items-center">
+              <h4 class="mt-0 mb-2 mr-auto">{{ item.title }}</h4>
             </div>
-            <template v-if="currentParticipation.can_manage_posts">
-                <Divider />
-                <Button class="p-button-sm px-2 py-1" @click="redirectToUpdate(item)">
-                    <i class="pi pi-pencil mr-2" style="font-size:0.9rem"></i>
-                    {{ $t('posts.editButton') }}
-                </Button>
-                <Button @click.prevent="deletePostHelper(item)" class="p-button-sm p-button-danger px-2 py-1 mr-2">
-                    <i class="pi pi-times mr-2" style="font-size:0.9rem"></i>
-                    {{ $t('posts.deleteButton') }}
-                </Button>
-            </template>
+            <span>{{ item.description }}</span>
+          </div>
         </div>
+        <Divider />
+        <div class="flex align-items-center text-sm">
+          <span class="time text-gray-500">{{
+            new Date(item.created_at) | dateFormat("YYYY.MM.DD")
+          }}</span>
+          <span class="files ml-auto"
+            >{{ item.attachments_count }} {{ $t("posts.filesAttached") }}</span
+          >
+        </div>
+      </div>
+      <template v-if="currentParticipation.can_manage_posts">
+        <Divider />
+        <Button class="p-button-sm px-2 py-1" @click="redirectToUpdate(item)">
+          <i class="pi pi-pencil mr-2" style="font-size: 0.9rem"></i>
+          {{ $t("posts.editButton") }}
+        </Button>
+        <Button
+          @click.prevent="deletePostHelper(item)"
+          class="p-button-sm p-button-danger px-2 py-1 mr-2"
+        >
+          <i class="pi pi-times mr-2" style="font-size: 0.9rem"></i>
+          {{ $t("posts.deleteButton") }}
+        </Button>
+      </template>
     </div>
+
+    <div ref="infinitescrolltrigger" />
+    <transition name="fade">
+      <div v-if="showloader" class="h-5rem flex justify-content-center">
+        <ProgressSpinner class="loader" />
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
-    import { mapActions, mapGetters } from 'vuex'
-    import Button from 'primevue/button'
-    import Divider from 'primevue/divider'
-    import ConfirmDialog from 'primevue/confirmdialog'
+import { mapActions, mapGetters, mapMutations } from "vuex";
+import Button from "primevue/button";
+import Divider from "primevue/divider";
+import ConfirmDialog from "primevue/confirmdialog";
+import Skeleton from "primevue/skeleton";
+import ProgressSpinner from "primevue/progressspinner";
 
-    export default {
-        props: {
-            type: String
+export default {
+  props: {
+    type: String,
+    items: Array,
+    isFetch: Boolean,
+    fetchItems: Function,
+    limit: Number,
+    offset: Number,
+  },
+
+  components: {
+    Button,
+    Divider,
+    ConfirmDialog,
+    Skeleton,
+    ProgressSpinner,
+  },
+
+  data: () => {
+    return {
+      showloader: false,
+    };
+  },
+
+  mounted() {
+    this.scrollTrigger();
+  },
+
+  computed: {
+    ...mapGetters({
+      canModerate: "rooms/canModerate",
+      currentParticipation: "participants/current",
+    }),
+  },
+
+  methods: {
+    ...mapActions({
+      deletePost: "roomposts/delete",
+    }),
+
+    async deletePostHelper(post) {
+      this.$confirm.require({
+        header: "Confirm the action",
+        message: "Do you really want to delete a post?",
+        icon: "pi pi-exclamation-triangle",
+        accept: () => {
+          this.deletePost(post);
         },
-
-        components: {
-            Button, Divider, ConfirmDialog
+        reject: () => {},
+      });
+    },
+    async redirectToUpdate(item) {
+      console.log(item);
+      this.$router.push({
+        name: "rooms.post.update",
+        params: {
+          postId: item.id,
+          roomId: item.room_id,
         },
-        
-        computed: {
-            ...mapGetters({
-                items: 'roomposts/items',
-                canModerate: 'rooms/canModerate',
-                currentParticipation: 'participants/current',
-            }),
-
-            typedPosts() {
-                return this.items.filter(e => e.type === this.type)
-            },
+      });
+    },
+    async redirectDetail(item) {
+      this.$router.push({
+        name: "rooms.post.detail",
+        params: {
+          postId: item.id,
+          roomId: item.room_id,
         },
+      });
+    },
+    fetchMore() {
+      this.$store.commit("roomposts/SET_IS_INFINITE_SCROLL", true);
 
-        methods: {
-            ...mapActions({
-                deletePost: 'roomposts/delete'
-            }),
+      if (this.type === "material") {
+        this.$store.commit("roomposts/SET_OFFSET_MATERIALS", this.offset + 5);
+      }
 
-            async deletePostHelper(post) {
-                this.$confirm.require({
-                    header: 'Confirm the action',
-                    message: 'Do you really want to delete a post?',
-                    icon: 'pi pi-exclamation-triangle',
-                    accept: () => {
-                        this.deletePost(post)
-                    },
-                    reject: () => {}
-                });
-            },
-            async redirectToUpdate(item) {
-                console.log(item)
-                this.$router.push({
-                    'name': 'rooms.post.update',
-                    params: {
-                        postId: item.id,
-                        roomId: item.room_id,
-                    }
-                })
-            },
-            async redirectDetail(item) {
-                this.$router.push({ 
-                    name: 'rooms.post.detail',
-                    params: {
-                        postId: item.id,
-                        roomId: item.room_id
-                    }
-                })
-            }
+      if (this.type === "homework") {
+        this.$store.commit("roomposts/SET_OFFSET_HOMEWORKS", this.offset + 5);
+      }
+
+      this.fetchItems(this.offset + 5, this.limit);
+      this.showloader = false;
+    },
+    scrollTrigger() {
+      const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && this.isFetch) {
+          this.showloader = true;
+          this.fetchMore();
         }
-    }
+      });
+      observer.observe(this.$refs.infinitescrolltrigger);
+    },
+  },
+};
 </script>
 
 <style scoped>
+.attachments-block {
+  height: 320px;
+  overflow: scroll;
+  padding-right: 10px;
+}
 .block-nav {
-    cursor: pointer;
+  cursor: pointer;
+}
+.loader {
+  width: 50px;
+  height: 50px;
 }
 </style>
--- store/roomposts.js
+++ store/roomposts.js
@@ -1,138 +1,270 @@
-import { apiClient } from "@/utils/api.js"
-import Cookies from 'js-cookie'
+import { apiClient } from "@/utils/api.js";
+import Cookies from "js-cookie";
 
 export const namespaced = true;
 
 export const state = () => ({
-    item: {},
-    items: [],
-    errors: {},
-})
+  item: {},
+  materialItems: [],
+  homeworkItems: [],
+  isFetchMaterials: false,
+  isFetchHomeworks: false,
+  isInfiniteScroll: true,
+  limitMaterials: 5,
+  offsetMaterials: 0,
+  limitHomeworks: 5,
+  offsetHomeworks: 0,
+  errors: {},
+});
 
 export const getters = {
-    item(state) {
-        return state.item;
-    },
-    items(state) {
-        return state.items
-    },
-    errors(state) {
-        return state.errors
-    }
-}
+  item(state) {
+    return state.item;
+  },
+  materialItems(state) {
+    return state.materialItems;
+  },
+  homeworkItems(state) {
+    return state.homeworkItems;
+  },
+  isFetchMaterials(state) {
+    return state.isFetchMaterials;
+  },
+  isFetchHomeworks(state) {
+    return state.isFetchHomeworks;
+  },
+  isInfiniteScroll(state) {
+    return state.isInfiniteScroll;
+  },
+  limitMaterials(state) {
+    return state.limitMaterials;
+  },
+  offsetMaterials(state) {
+    return state.offsetMaterials;
+  },
+  limitHomeworks(state) {
+    return state.limitHomeworks;
+  },
+  offsetHomeworks(state) {
+    return state.offsetHomeworks;
+  },
+  errors(state) {
+    return state.errors;
+  },
+};
 
 export const mutations = {
-    SET_ITEMS(state, items) {
-        state.items = items;
-    },
-    SET_ITEM(state, item) {
-        state.item = item
-    },
-    SET_ERRORS(state, items) {
-        state.errors = items
-    }
-}
+  SET_MATERIAL_ITEMS(state, items) {
+    state.materialItems = items;
+  },
+  SET_HOMEWORK_ITEMS(state, items) {
+    state.homeworkItems = items;
+  },
+  SET_IS_FETCH_MATERIALS(state, item) {
+    state.isFetchMaterials = item;
+  },
+  SET_IS_FETCH_HOMEWORKS(state, item) {
+    state.isFetchHomeworks = item;
+  },
+  SET_IS_INFINITE_SCROLL(state, item) {
+    state.isInfiniteScroll = item;
+  },
+  SET_OFFSET_MATERIALS(state, item) {
+    state.offsetMaterials = item;
+  },
+  SET_LIMIT_MATERIALS(state, item) {
+    state.limitMaterials = item;
+  },
+  SET_OFFSET_HOMEWORKS(state, item) {
+    state.offsetHomeworks = item;
+  },
+  SET_LIMIT_HOMEWORKS(state, item) {
+    state.limitHomeworks = item;
+  },
+  SET_ITEM(state, item) {
+    state.item = item;
+  },
+  SET_ERRORS(state, items) {
+    state.errors = items;
+  },
+};
 
 export const actions = {
-    async create({ commit }, requestBody) {
-        const client = await apiClient
-        const accessToken = this.$cookies.get('token')
-        let response = undefined
-        try {
-            response = await client.apis.roomPost.createRoomPost({}, {
-                requestInterceptor: (request) => {
-                    request.headers.Authorization = `Bearer ${accessToken}`
-                },
-                requestBody: requestBody,
-            })
-            commit('SET_ITEM', response.body)
-            commit('SET_ERRORS', {})
-        } catch (e) {
-            console.error(e.response)
-            commit('SET_ERRORS', e.response.body.detail)
-            response = e.response
+  async create({ commit }, requestBody) {
+    const client = await apiClient;
+    const accessToken = this.$cookies.get("token");
+    let response = undefined;
+    try {
+      response = await client.apis.roomPost.createRoomPost(
+        {},
+        {
+          requestInterceptor: (request) => {
+            request.headers.Authorization = `Bearer ${accessToken}`;
+          },
+          requestBody: requestBody,
         }
-        console.log(response)
-        return response
-    },
-    async fetch({ commit }, {roomId, search, ordering, limit, offset}) {
-        const client = await apiClient
-        const accessToken = this.$cookies.get('token')
-        
-        try {
-            const response = await client.apis.roomPost.getRoomPosts(
-                {
-                    room_id: roomId,
-                    search: search,
-                    ordering: ordering,
-                    limit: limit,
-                    offset: offset,
-                },
-                {
-                    requestInterceptor: (request) => {
-                        request.headers.Authorization = `Bearer ${accessToken}`
-                },
-            })
-            commit('SET_ITEMS', response.body.items)
-        } catch (e) {
-            await console.error(e.response)
-            throw e.response
-        }
-    },
-    async delete({ getters, commit }, roomPost) {
-        const client = await apiClient
-        const accessToken = this.$cookies.get('token')
-        
-        try {
-            await client.apis.roomPost.deleteRoomPost({
-                post_id: roomPost.id
-            }, {
-                requestInterceptor: (request) => {
-                    request.headers.Authorization = `Bearer ${accessToken}`
-                },
-            })
-            console.log(getters.items)
-            commit('SET_ITEMS', getters.items.filter(item => item.id != roomPost.id))
-        } catch (e) {
-            console.error(e)
-        }
-    },
-    async get({ commit }, roomPostId) {
-        const client = await apiClient
-        const accessToken = this.$cookies.get('token')
-
-        try {
-            const response = await client.apis.roomPost.getRoomPost({post_id: roomPostId},
-                {
-                    requestInterceptor: (request) => {
-                        request.headers.Authorization = `Bearer ${accessToken}`
-                    },
-                }
-            )
-            commit('SET_ITEM', response.body)
-        }
-        catch (e) {
-            console.error(e)
-        }
-    },
-    async update({ commit }, { postId, requestBody }) {
-        const client = await apiClient
-        const accessToken = this.$cookies.get('token')
-        
-        try {
-            const response = await client.apis.roomPost.updateRoomPost({
-                post_id: postId
-            }, {
-                requestInterceptor: (request) => {
-                    request.headers.Authorization = `Bearer ${accessToken}`
-                },
-                requestBody: requestBody,
-            })
-            commit('SET_ITEM', response.body)
-            commit('SET_ERRORS', {})
-        } catch (e) {
-            console.error(e.response.body)
-            commit('SET_ERRORS', e.response.body.detail)
-        }
+      );
+      commit("SET_ITEM", response.body);
+      commit("SET_ERRORS", {});
+    } catch (e) {
+      commit("SET_ERRORS", e.response.body.detail);
+      response = e.response;
     }
-}
+    console.log(response);
+    return response;
+  },
+  async fetchMaterials(
+    { commit, state },
+    { roomId, search, ordering, limit, offset }
+  ) {
+    const client = await apiClient;
+    const accessToken = this.$cookies.get("token");
+
+    try {
+      const response = await client.apis.roomPost.getRoomPosts(
+        {
+          room_id: roomId,
+          search: search,
+          ordering: ordering,
+          limit: limit,
+          offset: offset,
+          type: "material",
+        },
+        {
+          requestInterceptor: (request) => {
+            request.headers.Authorization = `Bearer ${accessToken}`;
+          },
+        }
+      );
+
+      commit(
+        "SET_MATERIAL_ITEMS",
+        state.isInfiniteScroll
+          ? [...state.materialItems, ...response.body.items]
+          : response.body.items
+      );
+
+      if (state.isInfiniteScroll) {
+        if (response.body.items.length < 5) {
+          commit("SET_IS_FETCH_MATERIALS", false);
+        } else {
+          commit("SET_IS_FETCH_MATERIALS", true);
+        }
+      }
+    } catch (e) {
+      await console.error(e.response);
+      throw e.response;
+    }
+  },
+  async fetchHomeworks(
+    { commit, state },
+    { roomId, search, ordering, limit, offset }
+  ) {
+    const client = await apiClient;
+    const accessToken = this.$cookies.get("token");
+
+    try {
+      const response = await client.apis.roomPost.getRoomPosts(
+        {
+          room_id: roomId,
+          search: search,
+          ordering: ordering,
+          limit: limit,
+          offset: offset,
+          type: "homework",
+        },
+        {
+          requestInterceptor: (request) => {
+            request.headers.Authorization = `Bearer ${accessToken}`;
+          },
+        }
+      );
+
+      commit(
+        "SET_HOMEWORK_ITEMS",
+        state.isInfiniteScroll
+          ? [...state.homeworkItems, ...response.body.items]
+          : response.body.items
+      );
+
+      if (state.isInfiniteScroll) {
+        if (response.body.items.length < 5) {
+          commit("SET_IS_FETCH_HOMEWORKS", false);
+        } else {
+          commit("SET_IS_FETCH_HOMEWORKS", true);
+        }
+      }
+    } catch (e) {
+      await console.error(e.response);
+      throw e.response;
+    }
+  },
+  async delete({ getters, commit }, roomPost) {
+    const client = await apiClient;
+    const accessToken = this.$cookies.get("token");
+
+    try {
+      await client.apis.roomPost.deleteRoomPost(
+        {
+          post_id: roomPost.id,
+        },
+        {
+          requestInterceptor: (request) => {
+            request.headers.Authorization = `Bearer ${accessToken}`;
+          },
+        }
+      );
+      commit(
+        "SET_MATERIAL_ITEMS",
+        getters.materialItems.filter((item) => item.id != roomPost.id)
+      );
+      commit(
+        "SET_HOMEWORK_ITEMS",
+        getters.homeworkItems.filter((item) => item.id != roomPost.id)
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  async get({ commit }, roomPostId) {
+    const client = await apiClient;
+    const accessToken = this.$cookies.get("token");
+
+    try {
+      const response = await client.apis.roomPost.getRoomPost(
+        { post_id: roomPostId },
+        {
+          requestInterceptor: (request) => {
+            request.headers.Authorization = `Bearer ${accessToken}`;
+          },
+        }
+      );
+      commit("SET_ITEM", response.body);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  async update({ commit }, { postId, requestBody }) {
+    const client = await apiClient;
+    const accessToken = this.$cookies.get("token");
+
+    try {
+      const response = await client.apis.roomPost.updateRoomPost(
+        {
+          post_id: postId,
+        },
+        {
+          requestInterceptor: (request) => {
+            request.headers.Authorization = `Bearer ${accessToken}`;
+          },
+          requestBody: requestBody,
+        }
+      );
+      commit("SET_ITEM", response.body);
+      commit("SET_ERRORS", {});
+    } catch (e) {
+      console.error(e.response.body);
+      commit("SET_ERRORS", e.response.body.detail);
+    }
+  },
+};
